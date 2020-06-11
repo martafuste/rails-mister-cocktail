0a03b93d1ae052a00f1ae077dfd6cdb19f0d1481
marta fuste <marta.fustelazcano@gmail.com>
2020-06-11 15:47:03 +0200

    bootstrap added

---
+++ app/controllers/cocktails_controller.rb
@@ -0,0 +1,61 @@
+class CocktailsController < ApplicationController
+
+   before_action :set_cocktail, only: [:show, :edit, :update, :destroy, :chef]
+
+
+
+  def index
+    @cocktails = Cocktail.all
+  end
+
+  # GET /restaurants/1
+  def show
+  end
+
+  # GET /restaurants/new
+  def new
+    @cocktail = Cocktail.new
+  end
+
+
+  # def edit
+  # end
+
+
+  def create
+    @cocktail = Cocktail.new(cocktail_params)
+
+    if @cocktail.save
+      redirect_to @cocktail, notice: 'Your cocktail is on the way....!'
+    else
+      render :new
+    end
+  end
+
+  # PATCH/PUT /restaurants/1
+  # def update
+  #   if @cocktail.update(cocktail_params)
+  #     redirect_to @cocktail, notice: 'The cocktail was successfully updated.'
+  #   else
+  #     render :edit
+  #   end
+  # end
+
+  # # DELETE /restaurants/1
+  # def destroy
+  #   @cocktail.destroy
+  #   redirect_to cocktails_url, notice: 'Your cocktail was deleted.'
+  # end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_cocktail
+      @cocktail = Cocktail.find(params[:id])
+    end
+
+    # Only allow a trusted parameter "white list" through.
+    def cocktail_params
+      params.require(:cocktail).permit(:name)
+    end
+  end
+
